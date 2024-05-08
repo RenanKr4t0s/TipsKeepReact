7a5ac6f7fca03645d83cdb78295c5d8a9d6bbb44
RenanKr4t0s <redrums88@gmail.com>
2024-05-08 17:34:02 -0300

    iniciando as atividades no repositorio

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <header>
+            <h1> Eu sou o aplicativo TipsKeeper</h1>
+        </header>
+    )
+    
+};
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note(){
+    return(
+        <div className="note">
+            <h1>Eu sou um título</h1>
+            <p> Eu sou um parágrafo do título</p>
+        </div>
+    )
+}
